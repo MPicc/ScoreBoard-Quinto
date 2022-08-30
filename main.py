854b39a9b7d2f660c44e3d75f389824544126a5f
Marco Piccinini <mpicc@outlook.it>
2022-08-30 03:06:30 +0200

    Final?

--- main.py
+++ main.py
@@ -34,29 +34,58 @@ def write_scorer_GUI(scorer):
 
 def create_player_list(offset):
     players: List = []
-    for i in range(0, 14):
-        with open("./data/out/" + str(i + offset) + ".txt", 'r', encoding="utf-8") as f:
-            players.append(str(i) + ". " + f.read())
+    for i in range(0, 16):
+        if (i == 0):
+            with open("./data/out/" + str(i + offset) + ".txt", 'r', encoding="utf-8") as f:
+                players.append(f.read())
+        else:
+            with open("./data/out/" + str(i + offset) + ".txt", 'r', encoding="utf-8") as f:
+                players.append(str(i) + ". " + f.read())
     return players
 
-def create_teams_textfile():
+def create_teams_textfile15():
 
     player_list = create_player_list(0)
-    player_list_osp = create_player_list(14)
+    player_list_osp = create_player_list(16)
+
+    with open("./data/out/32.txt", 'r', encoding="utf-8") as f:
+        allSCQ = f.read()
+    with open("./data/out/33.txt", 'r', encoding="utf-8") as f:
+        allOSP = f.read()
+
+    with open("./data/score/" + "PlayerListSCQ" + ".txt", 'w', encoding="utf-8") as f:
+        for i in range(1, 16):
+            f.write("\n" + "".join(player_list[i]))
+        f.write("\n\nAll. "+ allSCQ)
+
+    with open("./data/score/" + "PlayerListOSP" + ".txt", 'w', encoding="utf-8") as f:
+        for i in range(1, 16):
+            f.write("\n" + "".join(player_list_osp[i]))
+        f.write("\n\nAll. "+ allOSP)
+
+def create_teams_textfile13():
+
+    player_list = create_player_list(0)
+    player_list_osp = create_player_list(16)
+
+    with open("./data/out/32.txt", 'r', encoding="utf-8") as f:
+        allSCQ = f.read()
+    with open("./data/out/33.txt", 'r', encoding="utf-8") as f:
+        allOSP = f.read()
 
     with open("./data/score/" + "PlayerListSCQ" + ".txt", 'w', encoding="utf-8") as f:
         for i in range(1, 14):
             f.write("\n" + "".join(player_list[i]))
-        f.write("\n")
+        f.write("\n\nAll. "+ allSCQ)
 
     with open("./data/score/" + "PlayerListOSP" + ".txt", 'w', encoding="utf-8") as f:
         for i in range(1, 14):
             f.write("\n" + "".join(player_list_osp[i]))
-        f.write("\n")
+        f.write("\n\nAll. "+ allOSP)
 
 
 def check_status():
-    with open("./data/out/14.txt", 'r', encoding="utf-8") as f:
+    with open("./data/out/16.txt", 'r', encoding="utf-8") as f:
         out = f.read()
     with open("./data/out/0.txt", 'r', encoding="utf-8") as f:
         home = f.read()
@@ -67,7 +96,7 @@ def check_status():
     with open("./data/score/ospiti.txt", 'r', encoding="utf-8") as f:
         ospiti = f.read()
 
-    output = "\n" + home + " - " + out + "\n" + scq + " - " + ospiti + "\n" + period + "\n\n"
+    output = "\n" + home + " - " + out + "\n" + scq + " - " + ospiti + "\n" + period + "\n"
 
     return output
 
@@ -136,92 +165,18 @@ def quit():
 
 
 def serve_menu():
-    # it would be nice to clear the screen here too
-    print(check_status() +
-          "\n\n"
-          "S - Premi S se ha segnato il Quinto!!\n"
-          "D - Premi D per togliere un punto al Quinto :'(\n"
-          "O - Premi O se ha segnato la squadra ospite \n"
-          "P - Premi P per togliere un punto alla squadra ospite :-) \n"
-          "# - Scrivi il numero del periodo di gioco e premi invio!\n"
-          "\n"
-          "T - Premi T per caricare i dati delle squadre\n"
-          "R - Premi R per resettare tutto!\n"
-          "G - Premi G per avviare la GUI!\n"
-          "X - Premi X per uscire!")
-
-    options = 'abcdefghijk'
-
-    choice = raw_input('>>>').lower()  # just input in python 3
-    os.system('cls||clear')
-
-    if choice == 'x':
-        return sys.exit()
-
-    elif choice == 's':
-        print("Ho aggiunto un punto allo Sporting \n\n")
-        do_operation("scq.txt", 1)
-        scq_scorer_menu()
-        return serve_menu(),
-
-    elif choice == 'd':
-        print("Ho tolto un punto allo Sporting \n\n")
-        do_operation("scq.txt", -1)
-        return serve_menu(),
-
-    elif choice == 'o':
-        print("Ho aggiunto un punto agli altri \n\n")
-        do_operation("ospiti.txt", 1)
-        out_scorer_menu()
-        return serve_menu(),
-
-    elif choice == 'p':
-        print("Ho tolto un punto agli altri \n\n")
-        do_operation("ospiti.txt", -1)
-        return serve_menu(),
-
-    elif choice == 't':
-        read_teams()
-        print("Ho caricato i dati dei giocatori \n\n")
-        return serve_menu(),
-
-    elif choice == 'r':
-        print("Ho resettato tutto \n\n")
-        return serve_menu(),
-
-    elif choice == '1':
-        print("Comincia il primo tempo! \n\n")
-        change_period(1)
-        return serve_menu(),
-
-    elif choice == '2':
-        print("Comincia il secondo tempo! \n\n")
-        change_period(2)
-        return serve_menu(),
-
-    elif choice == '3':
-        print("Comincia il terzo tempo! \n\n")
-        change_period(3)
-        return serve_menu(),
-
-    elif choice == '4':
-        print("Comincia il quarto tempo! \n\n")
-        change_period(4)
-        return serve_menu(),
-
-    elif choice == 'g':
 
         player_list = create_player_list(0)
-        player_list_osp = create_player_list(14)
+        player_list_osp = create_player_list(16)
 
         print("GUI avviata! \n\n")
         
         # Choose a Theme for the Layout
-        sg.theme('DarkRed1')
+        sg.theme('SystemDefault')
        
-        layout = [[sg.Text('La nuova Scoreboard per le dirette del Quinto by Marco')],
-                [sg.Text(check_status(), key ='-RES-', justification='c')],
-                [sg.Button('Quinto'), sg.Button('Ospiti'), sg.Button('!Quinto'), sg.Button('!Ospiti'), sg.Button('Reset'), sg.Button('Load Data')],  # set options for item in listbox
+        layout = [[sg.Text('Scoreboard per dirette by Marco',font=("Arial",18))],
+                [sg.Text(check_status(), key ='-RES-', justification='c', font = ("Arial", 14))],
+                [sg.Button('!Quinto', size=(5,2)), sg.Button('Quinto', size=(15,2)), sg.Button('Ospiti', size=(15,2)), sg.Button('!Ospiti', size=(5,2))], #sg.Button('Load Data')],  # set options for item in listbox
                 [sg.Listbox(values = player_list,
                             size =(24, 16),
                             key ='-LISTscq-',
@@ -230,11 +185,13 @@ def serve_menu():
                             size =(24, 16),
                             key ='-LISTosp-',
                             enable_events = True)],
-                [sg.Text ('Allenatori: ')],
-                [sg.InputText(key ='allSCQ', size = (21,20)),sg.InputText(key ='allOSP', size = (21,20))],
+                # [sg.Text ('Allenatori: ')],
+                # [sg.InputText(key ='allSCQ', size = (21,20)),sg.InputText(key ='allOSP', size = (21,20))],
+                [sg.Text ('\n', font=("Arial",2))],                
                 [sg.Text ('Tempo: ')],
-                [sg.Slider(orientation ='horizontal', key='timesSlider', enable_events = True, size = (42,20), range=(1,4))],
-                [sg.Button('Exit'), sg.Button('Crea File Formazioni')]]
+                [sg.Combo (['1° Tempo', '2° Tempo','3° Tempo', '4° Tempo','1° T. Supp', '2° T. Supp', 'Tiri Rigore'], key='timesSlider', enable_events= True, size=(40,20))],
+                [sg.Text ('\n', font=("Arial",5))],
+                [sg.Button('Reset'), sg.Button ('Inserisci Giocatori'), sg.Button('Exit')]],
         
         window = sg.Window('SCQuinto Scoreboard Manager', layout, element_justification='c')
 
@@ -242,8 +199,11 @@ def serve_menu():
         while True:  
             event, values = window.read()
 
+            if (event == sg.WIN_CLOSED):
+                break
+
             if event in (None,'timesSlider'):
-                k = int(values['timesSlider'])
+                k = (values['timesSlider'])
                 change_period(k)
                 window['-RES-'].Update(check_status())
 
@@ -258,14 +218,8 @@ def serve_menu():
                 window['-RES-'].Update(check_status())
 
             if event in (None, 'Exit'):
+                quit()
                 break
-
-            if event in (None, 'Crea File Formazioni'):
-                create_teams_textfile()
-                with open("./data/score/" + "PlayerListSCQ" + ".txt", 'a', encoding="utf-8") as f:
-                    f.write("\nAll. " + values['allSCQ'])
-                with open("./data/score/" + "PlayerListOSP" + ".txt", 'a', encoding="utf-8") as f:
-                    f.write("\nAll. " + values['allOSP'])
 
             if event in (None, 'Quinto'):
                 do_operation("scq.txt", 1)
@@ -288,72 +242,104 @@ def serve_menu():
                 writenumber("scq.txt", 0)
                 window['-RES-'].Update(check_status())
 
+            if event in (None, 'Inserisci Giocatori'):
+                inserisci_giocatori()
+                player_list = create_player_list(0)
+                player_list_osp = create_player_list(16)
+                window['-LISTscq-'].Update(player_list)
+                window['-LISTosp-'].Update(player_list_osp)
+                window['-RES-'].Update(check_status())   
+
             if event in (None, 'Load Data'):
                 read_teams()
                 player_list = create_player_list(0)
-                player_list_osp = create_player_list(14)
+                player_list_osp = create_player_list(16)
                 window['-LISTscq-'].Update(player_list)
                 window['-LISTosp-'].Update(player_list_osp)
-                window['-RES-'].Update(check_status())                                
+                window['-RES-'].Update(check_status())           
                             
-        # Close
-        window.close()
+def inserisci_giocatori():
 
-        return serve_menu(),
+    print("GUI inserimento giocatori avviata! \n\n")
+    
+    # Choose a Theme for the Layout
+    sg.theme('SystemDefault')
 
+    loaded = []
+    for i in range (0,34):
+        with open("./data/out/" + str(i) + ".txt", 'r', encoding="utf-8") as f:
+            loaded.insert(i, f.read())
+        
+    
+    layout = [
+            [ sg.Text('Squadre'),       sg.InputText(key ='HomeTeam', size =        (21,20),  default_text= loaded[0]) ,            sg.InputText(key ='OSPTeam',         size = (21,20),  default_text= loaded[16] )],
+            [ sg.Text('1'),             sg.InputText(key ='SCQPlayer1', size =      (21,20),  default_text= loaded[1]),             sg.InputText(key ='OSPPlayer1',      size = (21,20),  default_text= loaded[17] )],
+            [ sg.Text('2'),             sg.InputText(key ='SCQPlayer2', size =      (21,20),  default_text= loaded[2]),             sg.InputText(key ='OSPPlayer2',      size = (21,20),  default_text= loaded[18] )],
+            [ sg.Text('3'),             sg.InputText(key ='SCQPlayer3', size =      (21,20),  default_text= loaded[3]),             sg.InputText(key ='OSPPlayer3',      size = (21,20),  default_text= loaded[19] )],
+            [ sg.Text('4'),             sg.InputText(key ='SCQPlayer4', size =      (21,20),  default_text= loaded[4]),             sg.InputText(key ='OSPPlayer4',      size = (21,20),  default_text= loaded[20] )],
+            [ sg.Text('5'),             sg.InputText(key ='SCQPlayer5', size =      (21,20),  default_text= loaded[5]),             sg.InputText(key ='OSPPlayer5',      size = (21,20),  default_text= loaded[21] )],
+            [ sg.Text('6'),             sg.InputText(key ='SCQPlayer6', size =      (21,20),  default_text= loaded[6]),             sg.InputText(key ='OSPPlayer6',      size = (21,20),  default_text= loaded[22] )],
+            [ sg.Text('7'),             sg.InputText(key ='SCQPlayer7', size =      (21,20),  default_text= loaded[7]),             sg.InputText(key ='OSPPlayer7',      size = (21,20),  default_text= loaded[23] )],
+            [ sg.Text('8'),             sg.InputText(key ='SCQPlayer8', size =      (21,20),  default_text= loaded[8]),             sg.InputText(key ='OSPPlayer8',      size = (21,20),  default_text= loaded[24] )],
+            [ sg.Text('9'),             sg.InputText(key ='SCQPlayer9', size =      (21,20),  default_text= loaded[9]),             sg.InputText(key ='OSPPlayer9',      size = (21,20),  default_text= loaded[25] )],
+            [ sg.Text('10'),            sg.InputText(key ='SCQPlayer10', size =     (21,20),  default_text= loaded[10]),             sg.InputText(key ='OSPPlayer10',     size = (21,20),  default_text= loaded[26] )],
+            [ sg.Text('11'),            sg.InputText(key ='SCQPlayer11', size =     (21,20),  default_text= loaded[11]),             sg.InputText(key ='OSPPlayer11',     size = (21,20),  default_text= loaded[27] )],
+            [ sg.Text('12'),            sg.InputText(key ='SCQPlayer12', size =     (21,20),  default_text= loaded[12]),             sg.InputText(key ='OSPPlayer12',     size = (21,20),  default_text= loaded[28] )],
+            [ sg.Text('13'),            sg.InputText(key ='SCQPlayer13', size =     (21,20),  default_text= loaded[13]),             sg.InputText(key ='OSPPlayer13',     size = (21,20),  default_text= loaded[29] )],
+            [ sg.Text('14'),            sg.InputText(key ='SCQPlayer14', size =     (21,20),  default_text= loaded[14]),             sg.InputText(key ='OSPPlayer14',     size = (21,20),  default_text= loaded[30] )],
+            [ sg.Text('15'),            sg.InputText(key ='SCQPlayer15', size =     (21,20),  default_text= loaded[15]),             sg.InputText(key ='OSPPlayer15',     size = (21,20),  default_text= loaded[31] )],
+            [ sg.Text('Allenatori'),    sg.InputText(key ='SCQ Allenatore', size =  (21,20),  default_text= loaded[32]),             sg.InputText(key ='OSP Allenatore',  size = (21,20),  default_text= loaded[33] )],
+            [ sg.Button('Annulla'), sg.Button('Carica e Crea Lista')]
+    ]
+    
+    window2 = sg.Window('Inserimento Formazioni', layout, element_justification='r')
 
-    else:
-        return serve_menu()
+    # This is an Event Loop
+    while True:  
+        event, values = window2.read()
 
-
-def scq_scorer_menu():
-    # it would be nice to clear the screen here too
-    print("\nChi ha segnato?")
-    player_list = create_player_list(0)
-    for value in player_list:
-        print(value + "")
-
-    choice = raw_input('>>>').lower()  # just input in python 3
-    choice = int(choice)
-
-    if int(choice)  in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]:
-        target_file = "./data/out/" + str(int(choice)) + ".txt"
-        update_last_scorer(target_file)
-    else:
-        print("ERRORE!! INPUT NON VALIDO!")
-        return
-
-    os.system('cls||clear')
-
-    if choice == 'x':
-        return sys.exit()
-
-
-def out_scorer_menu():
-    # it would be nice to clear the screen here too
-    print("\nChi ha segnato?")
-    player_list = create_player_list(0 + 14)
-    for value in player_list:
-        print(value + "")
-
-    choice = raw_input('>>>').lower()  # just input in python 3
-
-    if int(choice)  in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]:
-        target_file = "./data/out/" + str(int(choice)+14) + ".txt"
-        update_last_scorer(target_file)
-    else:
-        print("ERRORE!! INPUT NON VALIDO!")
-        return
+        if (event == sg.WIN_CLOSED):
+            window2.close()
+            break
 
 
-    os.system('cls||clear')
+        if (event in (None, 'Annulla')):
+            window2.close()
+            break
 
-    if choice == 'x':
-        return sys.exit()
+        if (event in (None, 'Carica e Crea Lista')):
+
+            with open("./data/out/" + "0.txt", 'w', encoding="utf-8") as f:
+                        f.write(values['HomeTeam'])
+            with open("./data/out/" + "16.txt", 'w', encoding="utf-8") as f:
+                        f.write(values['OSPTeam'])
+
+            for i in range(1, 16):
+                    with open("./data/out/" + str(i) + ".txt", 'w', encoding="utf-8") as f:
+                        f.write(values['SCQPlayer' + str(i)])
+            for i in range(1, 16):
+                    with open("./data/out/" + str(i+16) + ".txt", 'w', encoding="utf-8") as f:
+                        f.write(values['OSPPlayer' + str(i)])
+                
+            with open("./data/out/" + "32.txt", 'w', encoding="utf-8") as f:
+                        f.write(values['SCQ Allenatore'])
+            with open("./data/out/" + "33.txt", 'w', encoding="utf-8") as f:
+                        f.write(values['OSP Allenatore'])
+
+            create_teams_textfile13()
+
+            if (values['SCQPlayer14'].strip() != "" or values['SCQPlayer15'].strip() != "" or values['OSPPlayer14'].strip() != "" or values['OSPPlayer15'].strip() != ""):
+                create_teams_textfile15()
+
+            
+            window2.close()
+            break
+
+
 
 
 while True:
-    func = serve_menu()
-    func()
+    serve_menu()
+    break
 
 
